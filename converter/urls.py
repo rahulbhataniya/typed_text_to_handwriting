23c1747587fc6043b2b23a81f0b3bc2052190aa0
rahul <rahulbhataniya2468@gmail.com>
2021-01-20 19:49:03 +0530

    add signup login logout and home page

--- converter/urls.py
+++ converter/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
-urlpatterns=[path('',views.index,name='index'),
+
+app_name = 'converter'
+
+urlpatterns=[path("homepage", views.homepage, name="homepage"),
+                path('',views.index,name='index'),
                 path('pdf',views.download_file,name='download_file'),
                 path('train_upload',views.train_upload,name='train_upload'),
-                path('showimage',views.showimage,name='showimage')]
-
+                path('showimage',views.showimage,name='showimage'),
+                path('access_data',views.access_data,name='access_data'),
+                path("register", views.register, name="register"),
+                path("logout", views.logout_request, name="logout"),
+                path("login", views.login_request, name="login"),
+                ]
 
 #name of view use for pah to view in hyperlink ex  {{%url download_file %}}
--- converter/views.py
+++ converter/views.py
@@ -7,10 +7,15 @@ from converter.form import Uploadform
 import converter.txttohand
 import converter.input
 from . import txttohand
-
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from .models import Image
 from .form import ImageForm
+from django.contrib.auth import logout, authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
 
 
 def index(request):  
@@ -75,3 +80,66 @@ def showimage(request):
         current_form=ImageForm()
         context={'form':current_form}
         return render(request,'image.html',context)
+
+
+
+def access_data(request):
+    image_data= Image.objects.get(name=70)
+    print("Myoutput",image_data)
+    return render(request,'show_image.html',{'data': image_data})
+
+
+
+def homepage(request):
+    return render(request = request,template_name='home.html')
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New account created: {username}")
+            login(request, user)
+            return redirect("converter:homepage")
+
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+
+            return render(request = request,
+                          template_name = "register.html",
+                          context={"form":form})
+
+    form = UserCreationForm()
+    return render(request = request,
+                  template_name = "register.html",
+                  context={"form":form})
+
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("converter:homepage")
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request = request,
+                    template_name = "login.html",
+                    context={"form":form})
